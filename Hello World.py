246234eca12bc065a3995150b1c15f2a0575fc5c
Kaixuan <cqu_wkx@sina.cn>
2021-04-24 12:28:40 +0800

    modify the Theme

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,9 @@
-print('Hello World!!!!!!!\n12345')
+import pandas as pd
+a= [[111],[22222]]
+print(a)
+
+def run(name:str):
+    print('     this is run!!\t\nHello {}'.format(name))
+    return name
+
+run('VisualStudio2019')
